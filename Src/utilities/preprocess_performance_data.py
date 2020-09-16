45983df6e2aca83cece81c9f4f02e24fb7d46cfc
Catalina Obando <cobando85@gmail.com>
2020-09-16 15:11:29 +0200

    update

---
+++ Src/utilities/preprocess_performance_data.py
@@ -0,0 +1,91 @@
+# Imports
+import scipy.io
+import h5py
+import time
+from h5py import File  # Package used for loading data from the input h5 file
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+from os import listdir
+
+path_data_mat = os.path.normpath(os.path.join(os.path.dirname(__file__), '../../data/raw/'))
+path_data_save1 = os.path.normpath(os.path.join(os.path.dirname(__file__), '../../data/post_processed/Only_events/'))
+path_data_save2 = os.path.normpath(os.path.join(os.path.dirname(__file__), '../../data/post_processed/Performance/'))
+
+file_names = sorted(os.listdir(path=path_data_mat))
+file_names = [x for x in file_names if not x.startswith('.')]
+
+file_name = "VPja.mat"  # VPja.mat ; VPbax.mat
+
+for file_name in file_names:
+
+    # Extract data cnt - contains data of different channels:
+    # EEG,'EMGf', 'lead_gas', 'lead_brake', 'dist_to_lead', 'wheel_X', 'wheel_Y', 'gas', 'brake'
+    with h5py.File(os.path.join(path_data_mat, file_name), 'r') as f:
+        cnt = f.get('cnt')
+        x = np.array(cnt.get('x'))  # x is the continuous multivariate data
+
+    # Extract data mrk - contains information on the EVENTS
+    with h5py.File(os.path.join(path_data_mat, file_name), 'r') as f:
+        mrk = f.get('mrk')
+        time_mrk = np.array(mrk.get('time'))  # mrk.time is the timestamp for each event in milliseconds
+        y = np.array(mrk.get('y'))  # mrk.y is a binary matrix telling which of the five types each event is
+
+    car_brake_y = time_mrk.T * y[:, 1]  # contains the time in ms of events car_brake
+    react_emg_y = time_mrk.T*y[:,4]
+    car_collision_y = time_mrk.T*y[:,3]
+
+    events_y = np.array(car_brake_y[car_brake_y > 0] / 5).astype(int)
+    noevents_int = np.append(0, events_y)
+    noevents_len = np.append(events_y[0], np.diff(events_y))
+
+    car_brake_y_no0 = car_brake_y[car_brake_y>0]
+    react_emg_y_no0 = react_emg_y[react_emg_y>0]
+    react_time = []
+    event_noreact = []
+    for i in range(len(car_brake_y_no0)):
+        diff = react_emg_y_no0 - car_brake_y_no0[i]
+        if ( min(diff[diff>0]) > 10000):
+            event_noreact.append(i)
+        else:
+            react_time.append(min(diff[diff>0]))
+
+
+    events_react_y = np.delete(events_y,event_noreact)
+    perf = react_time / np.power(x[64,events_react_y],2)
+
+    # Construct matrix for all target segments for channel chn_name
+    ts_i = 60  # Target segment interval intial point -> corresponds to 300ms
+    ts_f = 260  # Target segment interval intial point -> corresponds to 1200ms
+    gap_sml = 600  # Non-targets data blocks (1500 ms duration, 500 ms equidistant offset) that were at least 3000 ms apart from any stimulus.
+    nts_offset = 100  # 500 ms equidistant offset equidistant offset
+
+    channels_eeg = np.arange(61)
+    channels_eeg = np.delete(channels_eeg, [0, 5])
+    for event_index in range(len(events_react_y)):
+        event_time = events_react_y[event_index]
+        A = x[channels_eeg, event_time - ts_i:event_time + ts_f]
+        A_norm = np.subtract(A, np.array(A[:, :20].mean(1)).reshape(A.shape[0], 1))
+        path_out = os.path.join(path_data_save1, '%s_segment_%d_event.npy' % (file_name, event_index))
+        path_out_perf = os.path.join(path_data_save2, '%s_segment_%d_performance.npy' % (file_name, event_index))
+        if os.path.isfile(path_out):
+            continue
+        np.save(path_out, A_norm)
+        np.save(path_out_perf, perf[event_index])
+
+
+
+
+    # gap_sml = 600  # 3000 ms apart from any stimulus
+    # nts_offset = 100  # 500 ms
+    # count = 0
+    # for event_index in range(len(noevents_len)):
+    #     noevents_seg = int((noevents_len[event_index] - gap_sml * 2 - ts_i + ts_f) / nts_offset)
+    #     if (noevents_seg > 0):
+    #         for noevent_ix in range(noevents_seg):
+    #             ip = noevents_int[event_index] + gap_sml + nts_offset * noevent_ix
+    #             fp = ip + ts_i + ts_f
+    #             A = x[channels_eeg, ip:fp]
+    #             A_norm = np.subtract(A, np.array(A[:, :20].mean(1)).reshape(A.shape[0], 1))
+    #             np.save(os.path.join(path_data_save, '%s_segment_%d_noevent.npy' % (file_name, count)), A_norm)
+    #             count += 1
